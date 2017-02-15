dd520d16096ab9888e72e71be0b01312dcbfc4be
Jerrgree <jrggb@yahoo.com>
2017-02-15 11:51:25 -0600

    Created initial function for stopword removal

--- stopword.R
+++ stopword.R
@@ -1,20 +1,30 @@
 library(tm)
 
 documents = c("She had toast for breakfast",
- "The coffee this morning was excellent", 
- "For lunch let's all have pancakes", 
- "Later in the day, there will be more talks", 
- "The talks on the first day were great", 
- "The second day should have good presentations too")
+              "The coffee this morning was excellent", 
+              "For lunch let's all have pancakes", 
+              "Later in the day, there will be more talks", 
+              "The talks on the first day were great", 
+              "The second day should have good presentations too")
 
-documents <- documents <- Corpus(VectorSource(documents))
-documents = tm_map(documents, content_transformer(tolower))
-documents = tm_map(documents, removePunctuation)
-documents = tm_map(documents, removeWords, stopwords("english"))
 
-documents[[1]]$content
-documents[[2]]$content
-documents[[3]]$content
-documents[[4]]$content
-documents[[5]]$content
-documents[[6]]$content
+stopRemove <- function(document)
+{
+  # Convert the character array to a corpus
+  document <- documents <- Corpus(VectorSource(document))
+  # Make all characters lowercase
+  document = tm_map(document, content_transformer(tolower))
+  #Strip all punctuation
+  document = tm_map(document, removePunctuation)
+  #Remove stopwords
+  document = tm_map(document, removeWords, stopwords("english"))
+  #Convert the corpus to a data frame
+  document <-data.frame(text=unlist(sapply(document, `[`, "content")), 
+                                 stringsAsFactors=F)
+  
+  return(document)
+}
+
+d <- stopRemove(documents)
+
+d
